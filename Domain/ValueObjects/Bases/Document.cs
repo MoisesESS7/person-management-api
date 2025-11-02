8b695191263b3d86217b24a5069c874420d87efb
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-01 23:34:10 -0300

    feat(domain): create base class Document with validation of number and date of birth

---
+++ Domain/ValueObjects/Bases/Document.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Exceptions.Documents;
+
+namespace Domain.ValueObjects.Bases
+{
+    public abstract class Document
+    {
+        public string Number { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        protected Document(string number, DateTime birthDate)
+        {
+            Number = number;
+            BirthDate = birthDate;
+            Validation();
+        }
+
+        private void Validation()
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(Number))
+                errors.Add("The document number cannot be empty.");
+
+            if (BirthDate == default)
+                errors.Add("The birth date can be informed.");
+
+            if (BirthDate.Date > DateTime.Now.Date)
+                errors.Add("The birth date cannot has in future.");
+
+            if(errors.Count > 0)
+                throw new DocumentValidationException(errors);
+        }
+    }
+}
