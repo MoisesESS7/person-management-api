95dd83089d90560eca5a74c1e5f6cc922a940f34
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-01 21:38:53 -0300

    freat: add specific exceptions for domain layer entities

---
+++ Domain/Exceptions/Documents/DocumentValidationException.cs
@@ -0,0 +1,11 @@
+﻿using Domain.ValueObjects.Bases;
+
+namespace Domain.Exceptions.Documents
+{
+    public class DocumentValidationException : DomainLayerException
+    {
+        public DocumentValidationException(IEnumerable<string> errors, Type? type = default) : base(errors, $"The {type?.Name ?? typeof(Document).Name} has validation errors.")
+        {
+        }
+    }
+}
