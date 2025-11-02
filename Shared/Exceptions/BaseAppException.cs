3b78525a523f02470a7811682aa52a3343e58320
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-01 21:32:21 -0300

    feat: Add a global exception for application-wide use

---
+++ Shared/Exceptions/BaseAppException.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Shared.Exceptions
+{
+    public class BaseAppException : Exception
+    {
+        public virtual int StatusCode { get; set; } = StatusCodes.Status500InternalServerError;
+        public virtual string Title { get; set; } = "Application error.";
+        public virtual string Type { get; set; } = "https://httpstatuses.com/500";
+        public IReadOnlyCollection<string> Errors { get; } = [];
+
+        public BaseAppException() { }
+        public BaseAppException(string? message) : base(message) { }
+        public BaseAppException(string? message, Exception? innerException) : base(message, innerException) { }
+
+        public BaseAppException(IEnumerable<string> errors, string message) : base(message)
+        {
+            Errors = errors != null && errors.Any()
+                ? errors.ToList().AsReadOnly()
+                : throw new ArgumentException("At least one error must be provided.", nameof(errors));
+        }
+
+        public override string ToString()
+        {
+            var details = Errors.Count > 0 ? string.Join("; ", Errors) : "No additional errors.";
+            return $"{base.ToString()}\nStatusCode: {StatusCode}, Title: {Title}, Type: {Type}, Errors: {details}";
+        }
+    }
+}
