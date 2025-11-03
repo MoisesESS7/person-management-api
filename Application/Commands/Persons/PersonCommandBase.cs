c7ef62110b2ee812202e14496861c40355bafaeb
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 18:23:00 -0300

    feat(application): add base class PersonCommandBase

---
+++ Application/Commands/Persons/PersonCommandBase.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Commands.Documents;
+
+namespace Application.Commands.Persons
+{
+    public abstract class PersonCommandBase
+    {
+        public string Name { get; private set; }
+        public CpfCommand Cpf { get; private set; }
+        public RgCommand Rg { get; private set; }
+
+        protected PersonCommandBase(string name, CpfCommand cpf, RgCommand rg)
+        {
+            Name = name;
+            Cpf = cpf;
+            Rg = rg;
+        }
+    }
+}
