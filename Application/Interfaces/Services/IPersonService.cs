c19af31457374a9645f5791cedfaf3c5b87128b5
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 19:03:09 -0300

    feat(application): add IPersonService interface definition

---
+++ Application/Interfaces/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Commands.Persons;
+using Application.Common.Response.Persons;
+
+namespace Application.Interfaces.Services
+{
+    public interface IPersonService
+    {
+        Task<IEnumerable<PersonResponse>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<PersonResponse> GetAsync(string value, CancellationToken cancellationToken = default);
+        Task<PersonResponse> CreateAsync(CreatePersonCommand command, CancellationToken cancellationToken = default);
+        Task<PersonResponse> UpdateAsync(UpdatePersonCommand command, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(string value, CancellationToken cancellationToken = default);
+    }
+}
