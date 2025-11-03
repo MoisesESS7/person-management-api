31ce820d896a2beb96a60ace611c323e043a779f
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 11:54:00 -0300

    feat(infrastructure): create DatabaseConnectionException exception for database connection errors

---
+++ Infrastructure/Exceptions/DatabaseConnectionException.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Exceptions
+{
+    public class DatabaseConnectionException : InfrastructureLayerException
+    {
+        public override int StatusCode => StatusCodes.Status503ServiceUnavailable;
+        public override string Title => "Database connection error";
+        public override string Type => "https://httpstatuses.com/503";
+
+        public DatabaseConnectionException() {}
+        public DatabaseConnectionException(string? message) : base(message) {}
+        public DatabaseConnectionException(string? message, Exception? innerException) : base(message, innerException) {}
+    }
+}
