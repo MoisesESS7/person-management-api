aa5ca05a47e47f78a199407dfc755221143df8fa
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 11:54:40 -0300

    feat(infrastructure): create DatabaseTimeoutException exception for timeout errors in database operations

---
+++ Infrastructure/Exceptions/DatabaseTimeoutException.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Exceptions
+{
+    public class DatabaseTimeoutException : InfrastructureLayerException
+    {
+        public override int StatusCode => StatusCodes.Status504GatewayTimeout;
+        public override string Title => "Database timed out error";
+        public override string Type => "https://httpstatuses.com/504";
+
+        public DatabaseTimeoutException() { }
+        public DatabaseTimeoutException(string? message) : base(message) { }
+        public DatabaseTimeoutException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
