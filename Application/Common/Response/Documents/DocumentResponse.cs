6b7a29b620d4c3beff0eb98865719ba31d981be8
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 18:28:41 -0300

    feat(application): add document response classes
    -Add DocumentResponse class
    -Create CpfResponse class
    -Add RgResponse class

---
+++ Application/Common/Response/Documents/DocumentResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Response.Documents
+{
+    public class DocumentResponse
+    {
+        public string Number { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        protected DocumentResponse(string number, DateTime birthDate)
+        {
+            Number = number;
+            BirthDate = birthDate;
+        }
+    }
+}
