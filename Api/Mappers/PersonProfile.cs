11c3db5d4ae0894c211ffacbb95ca7e10ff7c9ec
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-04 15:10:44 -0300

    feat(api): add PersonProfile for AutoMapper mappings between requests, commands, and responses

---
+++ Api/Mappers/PersonProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Application.Commands.Persons;
+using Application.Common.Commands.Documents;
+using Api.Requests.Persons;
+using AppResponse = Application.Common.Response.Persons.PersonResponse;
+using ApiResponse = Api.Responses.Persons.PersonResponse;
+
+namespace Api.Mappers
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<UpdatePersonRequest, UpdatePersonCommand>()
+                .ConstructUsing(src => new UpdatePersonCommand(src.Id, src.Name));
+
+            CreateMap<CreatePersonRequest, CreatePersonCommand>()
+                .ConstructUsing(src => new CreatePersonCommand(
+                    src.Name,
+                    new CpfCommand(src.CpfNumber, src.BirthDate, src.CpfRegistrationDate),
+                    new RgCommand(src.RgNumber, src.BirthDate, src.RgIssuingAuthority)
+                ));
+            
+            CreateMap<AppResponse, ApiResponse>()
+                .ConvertUsing(src => new ApiResponse
+                (
+                    src.Id,
+                    src.Name,
+                    src.Age,
+                    src.Rg.BirthDate,
+                    src.Cpf.Number,
+                    src.Cpf.RegistrationDate,
+                    src.Rg.Number,
+                    src.Rg.IssuingAuthority
+                ));
+        }
+    }
+}
