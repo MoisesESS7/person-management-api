1c9677d777f75df3b317601a6962a4ff4cc6ffc1
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-04 16:06:57 -0300

    feat(api): add API bootstrapper with AutoMapper and Swagger configuration

---
+++ Api/Bootstrapper.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.OpenApi.Models;
+using Api.Mappers;
+
+namespace Api
+{
+    public static class Bootstrapper
+    {
+        public static void AddApi(this IServiceCollection service, IConfiguration configuration)
+        {
+            // AutoMapper
+            service.AddAutoMapper(c =>
+            {
+                c.AddProfile(typeof(PersonProfile));
+            });
+
+            // Swagger
+            ConfigureSwagger(service, configuration);
+        }
+
+        private static void ConfigureSwagger(IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PDI - Implementing CRUD operations with MongoDB and creating GCP PUB/SUB",
+                    Version = "v1",
+                    Description = "Implement CRUD operations with MongoDB by creating indexes and aggregation, best practices with S.O.L.I.D (SRP, OCP), and GCP PUB/SUB.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "My name goes here - .NET Developer",
+                        Email = "exemple@outlook.com",
+                        Url = TryCreateUri(configuration["ContactUrl"])
+                    }
+                });
+
+                // This groups controllers using EndpointGroupName
+                options.TagActionsBy(api =>
+                {
+                    var groupName = api.GroupName;
+                    return [groupName ?? api.ActionDescriptor.RouteValues["controller"]];
+                });
+                options.DocInclusionPredicate((name, api) => { return true; });
+            });
+        }
+
+        private static Uri? TryCreateUri(string? url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return null;
+
+            return Uri.TryCreate(url, UriKind.Absolute, out var uri) ? uri : null;
+        }
+    }
+}
