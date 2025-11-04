f81b7a6936d1a6c3b49b25dc68e8ea8454f98031
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-04 14:40:17 -0300

    feat(api): add CreatePersonRequest model for person creation

---
+++ Api/Requests/Persons/CreatePersonRequest.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Enums;
+
+namespace Api.Requests.Persons
+{
+    public class CreatePersonRequest
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public string CpfNumber { get; private set; }
+        public DateTime CpfRegistrationDate { get; private set; }
+        public string RgNumber { get; private set; }
+        public InssuingAuthority RgIssuingAuthority { get; private set; }
+
+        public CreatePersonRequest(
+            string name,
+            DateTime birthDate,
+            string cpfNumber,
+            DateTime cpfRegistrationDate,
+            string rgNumber,
+            InssuingAuthority rgIssuingAuthority)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            CpfNumber = cpfNumber;
+            CpfRegistrationDate = cpfRegistrationDate;
+            RgNumber = rgNumber;
+            RgIssuingAuthority = rgIssuingAuthority;
+        }
+    }
+}
