d89bfe21e80ab2060d001c464bc76b4be1cbdb8b
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-17 15:19:46 -0300

    feat(application): add PageMeta class with metadata for paginated searches

---
+++ Application/Common/Models/PageMeta.cs
@@ -0,0 +1,31 @@
+﻿using Application.Exceptions;
+
+namespace Application.Common.Models
+{
+    public sealed class PageMeta
+    {
+        private int _pageNumber;
+
+        public long TotalRecords { get; private set; }
+        public int TotalPages => (int)Math.Ceiling(TotalRecords / (double)PageSize);
+        public int PageSize { get; private set; }
+        public int PageNumber
+        {
+            get { return _pageNumber; }
+            private set
+            {
+                if (value > TotalPages)
+                    throw new PageNotFoundException("The requested page does not exist.");
+
+                _pageNumber = value;
+            }
+        }
+
+        public PageMeta(int pageNumber, int pageSize, long totalRecords)
+        {
+            TotalRecords = totalRecords;
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+        }
+    }
+}
