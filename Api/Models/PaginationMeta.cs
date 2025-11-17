c1f5e907d55b613422fa570d8ea24109266347c1
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-17 15:51:43 -0300

    feat(api): add the PaginationMeta class for paginated search metadata with a parameterized constructor

---
+++ Api/Models/PaginationMeta.cs
@@ -0,0 +1,18 @@
+﻿namespace Api.Models
+{
+    public sealed class PaginationMeta
+    {
+        public int PageNumber { get; private set; }
+        public int PageSize { get; private set; }
+        public long TotalRecords { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginationMeta(int pageNumber, int pageSize, long totalRecords, int totalPages)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = totalPages;
+        }
+    }
+}
