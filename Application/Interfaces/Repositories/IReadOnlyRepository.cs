0cf4c8b7225f96b5e9f91e4b9efbaf1215e93249
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-17 15:45:21 -0300

    feat(application): add SearchPagedAsync method signature for paginated search in the IReadOnlyRepository<TEntity> interface

--- Application/Interfaces/Repositories/IReadOnlyRepository.cs
+++ Application/Interfaces/Repositories/IReadOnlyRepository.cs
@@ -1,11 +1,12 @@
-﻿using System.Linq.Expressions;
+﻿using Application.Common.Models;
+using System.Linq.Expressions;
 
 namespace Application.Interfaces.Repositories
 {
     public interface IReadOnlyRepository<TEntity>
     {
         IQueryable<TEntity> AsQueryable();
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<TEntity>> SeachPagedAsync(SearchParams searchParams, CancellationToken cancellationToken = default);
         Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken = default);
         Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken = default);
         Task<long> CountAsync(Expression<Func<TEntity, bool>>? expression = default, CancellationToken cancellationToken = default);
