a85107b02be113ec9cfa33b9f4c7b8ec3c0b914b
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-23 14:07:13 -0300

    refactor(shared): refactor class ResultOfT<T> to handle generic error lists

--- Shared/Results/ResultOfT.cs
+++ Shared/Results/ResultOfT.cs
@@ -4,12 +4,13 @@
     {
         public T? Value { get; }
 
-        private ResultOfT(bool isSuccess, T? value, Error? error) : base (isSuccess, error)
+        private ResultOfT(bool isSuccess, T? value, IReadOnlyCollection<Error> errors) : base(isSuccess, errors)
         {
             Value = value;
         }
 
-        public static ResultOfT<T> Ok(T value) => new (true, value, null);
-        public static new ResultOfT<T> Fail(Error error) => new (false, default, error);
+        public static ResultOfT<T> Ok(T value) => new(true, value, []);
+        public static new ResultOfT<T> Fail(Error error) => new(false, default, [error]);
+        public static new ResultOfT<T> Fail(IEnumerable<Error> errors) => new(false, default, [.. errors]);
     }
 }
