db4d173b7bc59cb38fda2f0c455e1bd638b13375
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-24 16:32:20 -0300

    feat(api): add ApiControllerBase to centralize conversion of Result to ActionResult

---
+++ Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Api.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Results;
+
+namespace Api.Controllers;
+
+[ApiController]
+public abstract class ApiControllerBase : ControllerBase
+{
+    protected ActionResult ToActionResult<T>(ResultOfT<T> result)
+        => ControllerExtensions.Map(this, result);
+
+    protected ActionResult ToActionResult(Result result)
+        => ControllerExtensions.Map(this, result);
+}
