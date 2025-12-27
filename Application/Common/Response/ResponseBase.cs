9df96287c30f081f7f180605b780caca79d1b1ee
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-27 12:23:25 -0300

    feat(application): add ResponseBase class

---
+++ Application/Common/Response/ResponseBase.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Response.Auditables;
+
+namespace Application.Common.Response
+{
+    public class ResponseBase
+    {
+        public string Id { get; private set; }
+        public AuditableEntityResponse Auditable { get; private set; }
+
+        public ResponseBase(string id, AuditableEntityResponse auditable)
+        {
+            Id = id;
+            Auditable = auditable;
+        }
+    }
+}
