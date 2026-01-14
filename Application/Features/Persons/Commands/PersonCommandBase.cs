b504ff0cee2c16631f9966809bd0dafb2ec00a2c
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 14:59:04 -0300

    feat(application): add PersonCommandBase class

---
+++ Application/Features/Persons/Commands/PersonCommandBase.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Enums;
+
+namespace Application.Features.Persons.Commands
+{
+    public abstract class PersonCommandBase
+    {
+        public string Name { get; private set; }
+        public DateTimeOffset BirthDate { get; private set; }
+        public string CpfNumber { get; private set; }
+        public DateTimeOffset CpfRegistrationDate { get; private set; }
+        public string RgNumber { get; private set; }
+        public InssuingAuthority RgIssuingAuthority { get; private set; }
+
+        protected PersonCommandBase(
+            string name,
+            DateTimeOffset birthDate,
+            string cpfNumber,
+            DateTimeOffset cpfRegistrationDate,
+            string rgNumber,
+            InssuingAuthority rgIssuingAuthority)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            CpfNumber = cpfNumber;
+            CpfRegistrationDate = cpfRegistrationDate;
+            RgNumber = rgNumber;
+            RgIssuingAuthority = rgIssuingAuthority;
+        }
+    }
+}
