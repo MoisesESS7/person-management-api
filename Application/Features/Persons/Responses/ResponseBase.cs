9850553e62827dca54df2ffae7ed550ceba6d0d8
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 14:56:03 -0300

    feat(application): add ResponseBase class

---
+++ Application/Features/Persons/Responses/ResponseBase.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Features.Persons.Responses
+{
+    public abstract class ResponseBase
+    {
+        public string Id { get; private set; }
+        public DateTimeOffset CreatedAt { get; private set; }
+        public DateTimeOffset? UpdatedAt { get; private set; }
+        public DateTimeOffset? DeletedAt { get; private set; }
+
+        protected ResponseBase(
+            string id,
+            DateTimeOffset createdAt,
+            DateTimeOffset? updatedAt,
+            DateTimeOffset? deletedAt)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            DeletedAt = deletedAt;
+        }
+    }
+}
