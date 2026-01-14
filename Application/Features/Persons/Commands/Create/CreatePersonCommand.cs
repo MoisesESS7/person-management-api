a636c03a8204d0caca893761553f148f50a7c5dc
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 15:16:13 -0300

    feat(application): add CreatePersonCommand class

---
+++ Application/Features/Persons/Commands/Create/CreatePersonCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Persons.Responses;
+using MediatR;
+using Shared.Enums;
+using Shared.Results;
+
+namespace Application.Features.Persons.Commands.Create
+{
+    public sealed class CreatePersonCommand : PersonCommandBase, IRequest<ResultOfT<PersonResponse>>
+    {
+        public CreatePersonCommand(
+            string name,
+            DateTimeOffset birthDate,
+            string cpfNumber,
+            DateTimeOffset cpfRegistrationDate,
+            string rgNumber,
+            InssuingAuthority rgIssuingAuthority)
+            : base(name, birthDate, cpfNumber, cpfRegistrationDate, rgNumber, rgIssuingAuthority)
+        {
+        }
+    }
+}
