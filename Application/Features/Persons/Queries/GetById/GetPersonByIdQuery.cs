6b01e045a6516589253f13ee80e66a50c93feec2
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 15:17:47 -0300

    feat(application): add GetPersonByIdQuery class

---
+++ Application/Features/Persons/Queries/GetById/GetPersonByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Persons.Responses;
+using MediatR;
+using Shared.Results;
+
+namespace Application.Features.Persons.Queries.GetById
+{
+    public sealed class GetPersonByIdQuery : IRequest<ResultOfT<PersonResponse>>
+    {
+        public string Id { get; private set; }
+
+        public GetPersonByIdQuery(string id)
+        {
+            Id = id ?? string.Empty;
+        }
+    }
+}
