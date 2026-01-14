9a5a2ba0eea1b9519cd9e3529e037f32bffb3033
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 14:56:55 -0300

    feat(application): add PersonResponse class

---
+++ Application/Features/Persons/Responses/PersonResponse.cs
@@ -0,0 +1,38 @@
+﻿using Shared.Enums;
+
+namespace Application.Features.Persons.Responses
+{
+    public sealed class PersonResponse : ResponseBase
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public DateTimeOffset BirthDate { get; private set; }
+        public string CpfNumber { get; private set; }
+        public DateTimeOffset CpfRegistrationDate { get; private set; }
+        public string RgNumber { get; private set; }
+        public InssuingAuthority RgIssuingAuthority { get; private set; }
+
+        public PersonResponse(
+            string id,
+            string name,
+            int age,
+            DateTimeOffset birthDate,
+            string cpfNumber,
+            DateTimeOffset cpfRegistrationDate,
+            string rgNumber,
+            InssuingAuthority rgIssuingAuthority,
+            DateTimeOffset createdAt,
+            DateTimeOffset? updatedAt,
+            DateTimeOffset? deletedAt)
+            : base (id, createdAt, updatedAt, deletedAt)
+        {
+            Name = name;
+            Age = age;
+            BirthDate = birthDate;
+            CpfNumber = cpfNumber;
+            CpfRegistrationDate = cpfRegistrationDate;
+            RgNumber = rgNumber;
+            RgIssuingAuthority = rgIssuingAuthority;
+        }
+    }
+}
