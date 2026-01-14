bab8fdb6afd9fd7f4b5cc30e027e5afb9ff01ace
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-14 15:24:34 -0300

    feat(application): add PagedSearchQueryHandler class

---
+++ Application/Features/Persons/Queries/PagedSearch/PagedSearchQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.Models;
+using Application.Features.Persons.Responses;
+using Application.Interfaces.Repositories;
+using Application.Mappers;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Shared.Results;
+
+namespace Application.Features.Persons.Queries.PagedSearch
+{
+    internal sealed class PagedSearchQueryHandler : IRequestHandler<PagedSearchQuery, ResultOfT<PagedResult<PersonResponse>>>
+    {
+        private readonly ILogger<PagedSearchQueryHandler> _logger;
+        private readonly IRepository<Person> _personRepository;
+
+        public PagedSearchQueryHandler(ILogger<PagedSearchQueryHandler> logger, IRepository<Person> personRepository)
+        {
+            _logger = logger;
+            _personRepository = personRepository;
+        }
+
+        public async Task<ResultOfT<PagedResult<PersonResponse>>> Handle(PagedSearchQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Getting all persons...");
+            var totalRecords = await _personRepository.CountAsync(cancellationToken: cancellationToken);
+
+            var pageMeta = new PageMeta(query.SearchParams.PageNumber, query.SearchParams.PageSize, totalRecords);
+
+            var persons = await _personRepository.SeachPagedAsync(query.SearchParams, cancellationToken);
+
+            var personList = Mapper.ToListPersonResponse(persons);
+
+            var paginationMeta = new PaginationMeta(pageMeta);
+            var pagedResult = new PagedResult<PersonResponse>(personList, paginationMeta);
+
+            _logger.LogInformation(
+                "Retrieved {Count} persons (page {Page}/{TotalPages})",
+                pagedResult.Data.Count,
+                query.SearchParams.PageNumber,
+                pageMeta.TotalPages
+            );
+
+            return ResultOfT<PagedResult<PersonResponse>>.Ok(pagedResult);
+        }
+    }
+}
