f5b250c2822037eb126c628224c5455f44e6b2c8
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-15 17:05:32 -0300

    feat(application): add CreatePersonCommandValidator class

---
+++ Application/Features/Persons/Commands/Create/CreatePersonCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Validations.Extensions;
+using FluentValidation;
+
+namespace Application.Features.Persons.Commands.Create
+{
+    public sealed class CreatePersonCommandValidator : AbstractValidator<CreatePersonCommand>
+    {
+        public CreatePersonCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .MustBeValidName();
+
+            RuleFor(x => x.BirthDate)
+                .MustBeValidBirthDate();
+
+            RuleFor(x => x.CpfNumber)
+                .MustBeValidCpfNumber();
+
+            RuleFor(x => x.CpfRegistrationDate)
+                .MustBeValidCpfRegistrationDate(x => x.BirthDate);
+
+            RuleFor(x => x.RgNumber)
+                .MustBeValidRgNumber();
+
+            RuleFor(x => x.RgIssuingAuthority)
+                .MustBeValidRgIssuingAuthority();
+        }
+    }
+}
