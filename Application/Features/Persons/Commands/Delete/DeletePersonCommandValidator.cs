d80e8263a08dbddaa3d6db5101af0a4f923a7c05
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-15 17:06:38 -0300

    feat(application): add DeletePersonCommandValidator class

---
+++ Application/Features/Persons/Commands/Delete/DeletePersonCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Validations.Extensions;
+using FluentValidation;
+
+namespace Application.Features.Persons.Commands.Delete
+{
+    public sealed class DeletePersonCommandValidator : AbstractValidator<DeletePersonCommand>
+    {
+        public DeletePersonCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .MustBeValidGuid();
+        }
+    }
+}
