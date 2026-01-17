0caab13d74eac57460fa6f34d2ed7b04ca148c7f
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2026-01-17 13:46:50 -0300

    refactor(domain): adjust custom domain exception classes to extend BusinessException

--- Domain/Exceptions/Persons/PersonValidationException.cs
+++ Domain/Exceptions/Persons/PersonValidationException.cs
@@ -1,6 +1,8 @@
-﻿namespace Domain.Exceptions.Persons
+﻿using Shared.Exceptions;
+
+namespace Domain.Exceptions.Persons
 {
-    public class PersonValidationException : DomainLayerException
+    public class PersonValidationException : BusinessException
     {
         public PersonValidationException(IEnumerable<string> errors) : base(errors, "The person entity has validation errors.")
         {
