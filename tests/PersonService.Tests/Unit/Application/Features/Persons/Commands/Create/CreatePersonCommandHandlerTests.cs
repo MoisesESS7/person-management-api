9fed26d95c5cb7cafecffa82639ce40dbe91e8d2
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-01 16:24:19 -0300

    test(person-service): add the CreatePersonCommandHandlerTests class with the dependencies

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Create/CreatePersonCommandHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using PersonService.Application.Features.Persons.Commands.Create;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Create
+{
+    [Trait("Class", "Unit")]
+    public sealed class CreatePersonCommandHandlerTests
+    {
+        private readonly CreatePersonCommandFixture _fixture;
+        private readonly CreatePersonCommandHandler _handler;
+
+        public CreatePersonCommandHandlerTests()
+        {
+            _fixture = new CreatePersonCommandFixture();
+            _handler = _fixture.CreateHandler();
+        }
+    }
+}
