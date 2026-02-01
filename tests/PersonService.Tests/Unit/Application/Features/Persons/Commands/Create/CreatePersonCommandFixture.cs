c577fcc026f755d111ffb92e6f7b2bfc8d318af8
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-01 15:54:40 -0300

    test(person-service): add the CreatePersonCommandFixture to create the handler in the creation resource

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Create/CreatePersonCommandFixture.cs
@@ -0,0 +1,11 @@
+﻿using PersonService.Application.Features.Persons.Commands.Create;
+using PersonService.Tests.Common.Fixtures;
+
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Create
+{
+    public class CreatePersonCommandFixture : HandlerFixtureBase<CreatePersonCommandHandler>
+    {
+        public CreatePersonCommandHandler CreateHandler() => new (LoggerMock.Object, RepositoryMock.Object);
+    }
+}
