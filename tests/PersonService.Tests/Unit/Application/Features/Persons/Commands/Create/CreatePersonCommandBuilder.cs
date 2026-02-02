f9bc0e99ad75404c086d528cb3ec2908d84cbec5
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-02 17:38:33 -0300

    test(person-service): allow nullable fields in CreatePersonCommandBuilder

--- tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Create/CreatePersonCommandBuilder.cs
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Create/CreatePersonCommandBuilder.cs
@@ -5,26 +5,26 @@ namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Create
 {
     public sealed class CreatePersonCommandBuilder
     {
-        private string _name = "John Doe";
+        private string? _name = "John Doe";
         private DateTimeOffset _birthDate = new(new DateTime(1990, 10, 10));
-        private string _cpfNumber = "12345678910";
+        private string? _cpfNumber = "12345678910";
         private DateTimeOffset _cpfRegistrationDate = new(new DateTime(1990, 11, 11));
-        private string _rgNumber = "123456789";
+        private string? _rgNumber = "123456789";
         private IssuingAuthority _rgIssuingAuthority = IssuingAuthority.SSP_SP;
 
-        public CreatePersonCommandBuilder WithName(string name)
+        public CreatePersonCommandBuilder WithName(string? name)
         {
             _name = name;
             return this;
         }
 
-        public CreatePersonCommandBuilder WithCpfNumber(string number)
+        public CreatePersonCommandBuilder WithCpfNumber(string? number)
         {
             _cpfNumber = number;
             return this;
         }
 
-        public CreatePersonCommandBuilder WithRgNumber(string number)
+        public CreatePersonCommandBuilder WithRgNumber(string? number)
         {
             _rgNumber = number;
             return this;
