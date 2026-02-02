287a935c209ea0a91c591130b3b518a5fcd39065
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-02 18:18:35 -0300

    test(person-service): add builder to create object of the UpdatePersonCommand class

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Update/UpdatePersonCommandBuilder.cs
@@ -0,0 +1,24 @@
+﻿using PersonService.Application.Features.Persons.Commands.Update;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Update
+{
+    public sealed class UpdatePersonCommandBuilder
+    {
+        private string? _id = Guid.NewGuid().ToString();
+        private string? _name = "John Doe";
+
+        public UpdatePersonCommandBuilder WithId(string? id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public UpdatePersonCommandBuilder WithName(string? name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public UpdatePersonCommand Build() => new(_id!, _name!);
+    }
+}
