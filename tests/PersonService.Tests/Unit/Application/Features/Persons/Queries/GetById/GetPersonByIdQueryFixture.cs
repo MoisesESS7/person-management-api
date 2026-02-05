52bd5dce83a7b57de3bd3b92e6f17be75dc6e39a
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-05 20:40:26 -0300

    test(person-service): add GetPersonByIdQueryFixture to create the handler for getting resource by id

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/GetById/GetPersonByIdQueryFixture.cs
@@ -0,0 +1,10 @@
+﻿using PersonService.Application.Features.Persons.Queries.GetById;
+using PersonService.Tests.Common.Fixtures;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.GetById
+{
+    public sealed class GetPersonByIdQueryFixture : HandlerFixtureBase<GetPersonByIdQueryHandler>
+    {
+        public GetPersonByIdQueryHandler CreateHandler() => new(LoggerMock.Object, RepositoryMock.Object);
+    }
+}
