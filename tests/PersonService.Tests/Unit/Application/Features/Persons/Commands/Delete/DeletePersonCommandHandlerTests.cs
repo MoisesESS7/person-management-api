571887dd227a04c41a94dd72758580d2646bf907
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-05 19:03:58 -0300

    test(person-seervice): delete person when person exists

--- tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Delete/DeletePersonCommandHandlerTests.cs
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Delete/DeletePersonCommandHandlerTests.cs
@@ -1,4 +1,8 @@
-﻿using PersonService.Application.Features.Persons.Commands.Delete;
+﻿using FluentAssertions;
+using Moq;
+using PersonService.Application.Features.Persons.Commands.Delete;
+using PersonService.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Delete
 {
@@ -13,5 +17,37 @@ namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Delete
             _fixture = new DeletePersonCommandFixture();
             _handler = _fixture.CreateHandler();
         }
+
+        [Fact]
+        public async Task Handle_Should_Delete_Person_When_Person_Exists()
+        {
+            // Arrange
+            var command = new DeletePersonCommand(Guid.NewGuid().ToString());
+
+            _fixture.RepositoryMock
+                .Setup(r => r.ExistsAsync(
+                    It.IsAny<Expression<Func<Person, bool>>>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            _fixture.RepositoryMock
+                .Setup(r => r.DeleteAsync(
+                    It.IsAny<Expression<Func<Person, bool>>>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+
+            _fixture.RepositoryMock.Verify(r => r.ExistsAsync(
+                It.IsAny<Expression<Func<Person, bool>>>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            _fixture.RepositoryMock.Verify(r => r.DeleteAsync(
+                It.IsAny<Expression<Func<Person, bool>>>(), It.IsAny<CancellationToken>()), Times.Once);
+        }   
     }
 }
