3c6a80fe847b809cee713c2595482ddab4a11d87
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-05 17:46:34 -0300

    test(person-service): add the DeletePersonCommandHandlerTests class with dependencies

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Commands/Delete/DeletePersonCommandHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using PersonService.Application.Features.Persons.Commands.Delete;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Commands.Delete
+{
+    [Trait("Category", "Unit")]
+    public sealed class DeletePersonCommandHandlerTests
+    {
+        private readonly DeletePersonCommandFixture _fixture;
+        private readonly DeletePersonCommandHandler _handler;
+
+        public DeletePersonCommandHandlerTests()
+        {
+            _fixture = new DeletePersonCommandFixture();
+            _handler = _fixture.CreateHandler();
+        }
+    }
+}
