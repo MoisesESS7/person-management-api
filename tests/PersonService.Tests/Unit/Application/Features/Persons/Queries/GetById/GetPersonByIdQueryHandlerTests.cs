75720acb1463ff4cb73426bbcafcfce2971a3bce
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-05 20:41:15 -0300

    test(person-service): add the GetPersonByIdQueryHandlerTests class with dependencies

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/GetById/GetPersonByIdQueryHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using PersonService.Application.Features.Persons.Queries.GetById;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.GetById
+{
+    public sealed class GetPersonByIdQueryHandlerTests
+    {
+        private readonly GetPersonByIdQueryFixture _fixture;
+        private readonly GetPersonByIdQueryHandler _handler;
+
+        public GetPersonByIdQueryHandlerTests()
+        {
+            _fixture = new GetPersonByIdQueryFixture();
+            _handler = _fixture.CreateHandler();
+        }
+    }
+}
