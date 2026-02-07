2c41b2b2b980c16cf382d596c1aca24aba46b897
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-07 14:30:36 -0300

    test(person-service): add test to return person when person exists by id

--- tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/GetById/GetPersonByIdQueryHandlerTests.cs
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/GetById/GetPersonByIdQueryHandlerTests.cs
@@ -1,4 +1,9 @@
-﻿using PersonService.Application.Features.Persons.Queries.GetById;
+﻿using FluentAssertions;
+using Moq;
+using PersonService.Application.Features.Persons.Queries.GetById;
+using PersonService.Domain.Entities;
+using PersonService.Tests.Common.Builders;
+using System.Linq.Expressions;
 
 namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.GetById
 {
@@ -11,6 +16,37 @@ namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.GetById
         {
             _fixture = new GetPersonByIdQueryFixture();
             _handler = _fixture.CreateHandler();
+        }
+
+        [Fact]
+        public async Task Handle_Should_Return_Person_When_Person_Exists()
+        {
+            // Arrange
+            var person = new PersonBuilder().Build();
+            var query = new GetPersonByIdQuery(person.Id!);
+
+            _fixture.RepositoryMock
+                .Setup(r => r.GetAsync(
+                    It.Is<Expression<Func<Person, bool>>>(expr =>
+                        expr.Compile().Invoke(person)),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(person);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+            result.Value.Id.Should().Be(query.Id);
+            result.Value.Name.Should().Be(person.Name);
+
+            _fixture.RepositoryMock
+                .Verify(r => r.GetAsync(
+                    It.Is<Expression<Func<Person, bool>>>(expr =>
+                        expr.Compile().Invoke(person)),
+                    It.IsAny<CancellationToken>()),
+                    Times.Once);
         }
     }
 }
