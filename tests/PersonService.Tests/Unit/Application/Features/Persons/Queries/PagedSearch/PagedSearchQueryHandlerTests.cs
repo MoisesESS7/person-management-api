637478dd54c2bf2214e3677260194550443878ee
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-07 15:22:13 -0300

    test(person-service): add the PagedSearchQueryHandlerTests class with dependencies

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/PagedSearch/PagedSearchQueryHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using PersonService.Application.Features.Persons.Queries.PagedSearch;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.PagedSearch
+{
+    public sealed class PagedSearchQueryHandlerTests
+    {
+        private readonly PagedSearchQueryFixture _fixture;
+        private readonly PagedSearchQueryHandler _handler;
+
+        public PagedSearchQueryHandlerTests()
+        {
+            _fixture = new PagedSearchQueryFixture();
+            _handler = _fixture.CreateHandler();
+        }
+    }
+}
