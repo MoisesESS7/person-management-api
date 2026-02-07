b7847a61d76e72844d88496b84c3c1ec3e14fb1f
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-07 15:23:07 -0300

    test(person-service): add builder to create object of the PagedSearchQuery class

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/PagedSearch/PagedSearchQueryBuilder.cs
@@ -0,0 +1,56 @@
+﻿using PersonService.Application.Common.Models;
+using PersonService.Application.Features.Persons.Queries.PagedSearch;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.PagedSearch
+{
+    public sealed class PagedSearchQueryBuilder
+    {
+        private int _pageNumber = 1;
+        private int _pageSize = 25;
+        private string? _searchTerm = "John Doe";
+        private string? _sortBy = "name";
+        private bool _sortDescending;
+
+        public PagedSearchQueryBuilder WithPageNumber(int pageNumber)
+        {
+            _pageNumber = pageNumber;
+            return this;
+        }
+
+        public PagedSearchQueryBuilder WithPageSize(int pageSize)
+        {
+            _pageSize = pageSize;
+            return this;
+        }
+
+        public PagedSearchQueryBuilder WithSearchTerm(string? searchTerm)
+        {
+            _searchTerm = searchTerm;
+            return this;
+        }
+
+        public PagedSearchQueryBuilder WithSortBy(string sortBy)
+        {
+            _sortBy = sortBy;
+            return this;
+        }
+
+        public PagedSearchQueryBuilder WithSortDescending()
+        {
+            _sortDescending = true;
+            return this;
+        }
+
+        public PagedSearchQuery Build()
+        {
+            return new PagedSearchQuery(new SearchParams
+            {
+                PageNumber = _pageNumber,
+                PageSize = _pageSize,
+                SearchTerm = _searchTerm,
+                SortBy = _sortBy!,
+                SortDescending = _sortDescending
+            });
+        }
+    }
+}
