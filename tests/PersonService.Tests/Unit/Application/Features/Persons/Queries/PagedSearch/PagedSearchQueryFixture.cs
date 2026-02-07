324d2b1890e06809e1639d0c656a4613af56f3e4
MoisesESS7 <moisessilvagsp@outlook.com>
2026-02-07 15:20:38 -0300

    test(person-service): add the PagedSearchQueryFixture to create the handler

---
+++ tests/PersonService.Tests/Unit/Application/Features/Persons/Queries/PagedSearch/PagedSearchQueryFixture.cs
@@ -0,0 +1,10 @@
+﻿using PersonService.Application.Features.Persons.Queries.PagedSearch;
+using PersonService.Tests.Common.Fixtures;
+
+namespace PersonService.Tests.Unit.Application.Features.Persons.Queries.PagedSearch
+{
+    public sealed class PagedSearchQueryFixture : HandlerFixtureBase<PagedSearchQueryHandler>
+    {
+        public PagedSearchQueryHandler CreateHandler() => new (LoggerMock.Object, RepositoryMock.Object);
+    }
+}
